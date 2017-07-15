6f34d2c66cb505b71ba40469fb248689472be358
Jens Greiner <jens.greiner@me.com>
2017-07-15 18:35:00 +0200

    added list handling and LoaderManager methods to InventoryActivity and fixed activity_inventory.xml layout issues

--- app/src/main/java/com/greiner_co/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/greiner_co/inventoryapp/InventoryActivity.java
@@ -1,12 +1,27 @@
 package com.greiner_co.inventoryapp;
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class InventoryActivity extends AppCompatActivity {
+import com.greiner_co.inventoryapp.data.ProductContract.ProductEntry;
+
+public class InventoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String LOG_TAG = InventoryActivity.class.getSimpleName();
+    private static final int PRODUCT_LOADER = 0;
+
+    private ProductCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +36,46 @@ public class InventoryActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ListView productListView = (ListView) findViewById(R.id.products_list);
+
+        View emptyView = findViewById(R.id.empty_view);
+        productListView.setEmptyView(emptyView);
+
+        mCursorAdapter = new ProductCursorAdapter(this, null);
+        productListView.setAdapter(mCursorAdapter);
+
+        productListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(InventoryActivity.this, EditorActivity.class);
+                Uri uri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, id);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        });
+
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_PRODUCT_PRICE,
+                ProductEntry.COLUMN_PRODUCT_AMOUNT
+        };
+        return new CursorLoader(this, ProductEntry.CONTENT_URI, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
     }
 }
