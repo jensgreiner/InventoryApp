c61163b68525239d7a61ae2ba949f871df2f85f4
Jens Greiner <jens.greiner@me.com>
2017-07-15 12:48:50 +0200

    added ContentProvider to Manifest and fixed some analyzer findings in Manifest regarding backup and indexing

---
+++ app/src/main/java/com/greiner_co/inventoryapp/data/ProductProvider.java
@@ -0,0 +1,48 @@
+package com.greiner_co.inventoryapp.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * @link ContentProvider} for Inventory App.
+ * Created by Jens Greiner on 15.07.17.
+ */
+
+public class ProductProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
