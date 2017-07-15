30bacafa2aaf2026cae1ab77f4a2f21c2b004120
Jens Greiner <jens.greiner@me.com>
2017-07-15 14:45:00 +0200

    added insert(...) content and delete(...) content in ProductProvider.java

--- app/src/main/java/com/greiner_co/inventoryapp/data/ProductProvider.java
+++ app/src/main/java/com/greiner_co/inventoryapp/data/ProductProvider.java
@@ -9,6 +9,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.greiner_co.inventoryapp.data.ProductContract.ProductEntry;
 
@@ -82,16 +83,82 @@ public class ProductProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                return insertProduct(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    private Uri insertProduct(Uri uri, ContentValues contentValues) {
+        String name = contentValues.getAsString(ProductEntry.COLUMN_PRODUCT_NAME);
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Product requires a name");
+        }
+
+        Float price = contentValues.getAsFloat(ProductEntry.COLUMN_PRODUCT_PRICE);
+        if (price == null || price.isNaN() || price < 0) {
+            throw new IllegalArgumentException("Product requires a non-negative price");
+        }
+
+        Integer amount = contentValues.getAsInteger(ProductEntry.COLUMN_PRODUCT_AMOUNT);
+        if (amount == null || amount < 0) {
+            throw new IllegalArgumentException("Product requires a non-negative amount in stock");
+        }
+
+        String supplier = contentValues.getAsString(ProductEntry.COLUMN_PRODUCT_SUPPLIER);
+        if (supplier == null || supplier.isEmpty()) {
+            throw new IllegalArgumentException("Product requires a supplier");
+        }
+
+        String image = contentValues.getAsString(ProductEntry.COLUMN_PRODUCT_IMAGE);
+        if (image == null || image.isEmpty()) {
+            throw new IllegalArgumentException("Product requires an image");
+        }
+
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        long id = database.insert(ProductEntry.TABLE_NAME, null, contentValues);
+
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+        if (getContext() != null) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return ContentUris.withAppendedId(uri, id);
     }
 
     @Override
-    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
-        return 0;
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                return database.delete(ProductEntry.TABLE_NAME, selection, selectionArgs);
+            case PRODUCT_ID:
+                selection = ProductEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                int numberOfRows = database.delete(ProductEntry.TABLE_NAME, selection, selectionArgs);
+                if (numberOfRows > 0) {
+                    if (getContext() != null) {
+                        getContext().getContentResolver().notifyChange(uri, null);
+                    }
+                }
+                return numberOfRows;
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
     }
 
     @Override
-    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String selection, @Nullable String[] selectionArgs) {
         return 0;
     }
 }
