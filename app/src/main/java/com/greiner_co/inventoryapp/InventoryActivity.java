f6b338260fdfc0781480c0fdb6c4ac4086098b13
Jens Greiner <jens.greiner@me.com>
2017-07-15 12:26:42 +0200

    Initial commit

---
+++ app/src/main/java/com/greiner_co/inventoryapp/InventoryActivity.java
@@ -0,0 +1,13 @@
+package com.greiner_co.inventoryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+    }
+}
