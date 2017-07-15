695dabb4ce434f4e0d9b38e1cb8d75ae0fe97176
Jens Greiner <jens.greiner@me.com>
2017-07-15 14:01:45 +0200

    added custom ProductDbHelper.java extending SQLiteOpenHelper and filled onCreate() and onUpdate() methods

---
+++ app/src/main/java/com/greiner_co/inventoryapp/data/ProductDbHelper.java
@@ -0,0 +1,44 @@
+package com.greiner_co.inventoryapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.greiner_co.inventoryapp.data.ProductContract.ProductEntry;
+
+/**
+ * Custom SQLiteOpenHelper class to create a database if it not exists yet
+ * Created by Jens Greiner on 15.07.17.
+ */
+
+public class ProductDbHelper extends SQLiteOpenHelper {
+
+    private static final String LOG_TAG = ProductDbHelper.class.getSimpleName();
+    private static final String DATABASE_NAME = "inventory.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public ProductDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PRODUCTS_TABLE = "CREATE TABLE " + ProductEntry.TABLE_NAME + " ("
+                + ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_PRICE + " REAL NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_AMOUNT + " INTEGER NOT NULL DEFAULT 0, "
+                + ProductEntry.COLUMN_PRODUCT_SUPPLIER + " TEXT NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_IMAGE + " TEXT NOT NULL DEFAULT 'default_image');";
+
+        Log.v(LOG_TAG, SQL_CREATE_PRODUCTS_TABLE);
+
+        db.execSQL(SQL_CREATE_PRODUCTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        // nothing to do here, database is still in version 1
+    }
+}
