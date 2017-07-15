90a4c3723f8d5d3194254c147e82d7708f4461f9
Jens Greiner <jens.greiner@me.com>
2017-07-15 18:06:13 +0200

    added the EditorActivity to be finalized later

---
+++ app/src/main/java/com/greiner_co/inventoryapp/EditorActivity.java
@@ -0,0 +1,28 @@
+package com.greiner_co.inventoryapp;
+
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jens Greiner on 15.07.17.
+ */
+
+public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+}
