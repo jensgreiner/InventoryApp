8249258313d915e6f951752c3b969c7592ab49a1
Jens Greiner <jens.greiner@me.com>
2017-07-15 18:05:14 +0200

    modified activity_inventory to host a listView and added a list_item.xml layout

--- app/src/main/java/com/greiner_co/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/greiner_co/inventoryapp/InventoryActivity.java
@@ -1,7 +1,10 @@
 package com.greiner_co.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class InventoryActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class InventoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InventoryActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
