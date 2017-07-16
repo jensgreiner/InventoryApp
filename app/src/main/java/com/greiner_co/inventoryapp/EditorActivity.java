456a8de163c2fadc9535cf06af7b4f9ca4fb76db
Jens Greiner <jens.greiner@me.com>
2017-07-16 11:44:31 +0200

    added a bunch of internal app icons and added menu to editorActivity which differentiates between adding a new product or editing an existing product with showing or not showing the delete button, adjusted the FAB size and icon, adjusted the empty listView image

--- app/src/main/java/com/greiner_co/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/greiner_co/inventoryapp/EditorActivity.java
@@ -3,8 +3,12 @@ package com.greiner_co.inventoryapp;
 import android.app.LoaderManager;
 import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Allows the user to create a new product or edit an existing one.
@@ -14,6 +18,47 @@ import android.support.v7.app.AppCompatActivity;
 public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     private static final String LOG_TAG = EditorActivity.class.getSimpleName();
     private static final int EXISTING_PRODUCT_LOADER = 0;
+
+    private Uri mCurrentProductUri;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        // Check whether this is an edit call or an add call
+        mCurrentProductUri = getIntent().getData();
+        if (mCurrentProductUri == null) {
+            setTitle(R.string.editor_activity_title_add_product);
+
+            // TODO: Handle options menu here
+        } else {
+            setTitle(R.string.editor_activity_title_edit_product);
+
+            // TODO: Handle LoadManager here
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // Check for new product or edit product mode
+        if (mCurrentProductUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
